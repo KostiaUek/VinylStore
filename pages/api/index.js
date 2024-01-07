85f3a0f3b34789e73950b35f5b365156585c2de1
Kostia <231768@student.uek.krakow.pl>
2024-01-07 19:28:54 +0100

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -13,15 +13,15 @@ const functions = {
 
 // app.use(cors());
 // Example: http://localhost:1111/getimage?author=<AUTHOR_NAME_HERE>&track=<TRACK_NAME_HERE>
-app.get('/getimage', (req, res) => {
+app.get('/api/getimage', (req, res) => {
   const author = req.query.author;
   const track = req.query.track;
-
+  
   res.header("Access-Control-Allow-Origin", "*");
   Promise.resolve(functions.getImage(author, track)).then((result)=> res.send(result));
 });
 
-app.get('/getArtistsAndNames', (req, res) => {
+app.get('/api/getArtistsAndNames', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   Promise.resolve(functions.getArtistsAndNames()).then((result)=> res.send(result));
 });
