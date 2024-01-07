299879f85e117aff0490b5a615c4009aed641b2e
Kostia <231768@student.uek.krakow.pl>
2024-01-07 20:24:06 +0100

    Update

--- index.js
+++ index.js
@@ -1,86 +1,40 @@
-async function loadArtistList() {
-    const responce = await fetch("http://localhost:1111/getArtistsAndNames");
-    const data = await responce.json();
-    return data;
-}
+const express = require('express');
+// const cors = require('cors');
+const trackApi = require('./api/track/track.js')
+const mockarooApi = require("./api/mockaroo/mockaroo.js")
+const app = express();
+const port = 4528;
 
-async function getImage(artist, song) {
-    const responce = await fetch(`http://localhost:1111/getimage?author=${artist}&track=${song}`);
-    const text = responce.text();
-    return text;
-}
+// Defining functions
+const functions = {
+  getImage: (author, track) => trackApi.getImageFromTrackName(author, track),
+  getArtistsAndNames: () => mockarooApi.getArtistsAndTracksList(),
+};
 
-function card(song_data) {
-    return `
-        <div class="grid__item">
-            <div class="grid__item-flex shadow-s">
-                <div class="grid__preview" id="${song_data.song}_image"></div>
-                <div class="grid__title">
-                    <h2 class="grid__title-name">${song_data.song}</h2>
-                    <h3 class="grid__title-author">${song_data.name}</h3>
-                </div>
-            </div>
-            <div class="grid__overlay">
-                <div class="grid__overlay-items">
-                    <h2 class="grid__overlay-price">
-                        ${song_data.price}.99$
-                    </h2>
-                    <button class="grid__overlay-buy">
-                        Buy
-                    </button>
-                </div>
-            </div>
-        </div>
-    `
-}
 
-// fill
-function fill_grid(row_element, data) {
-    let result = "";
-    data.forEach(element => {
-        result += card(element);
-    });
+// Test message
+app.get("/api/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
-    row_element.innerHTML = result;
-}
+// app.use(cors());
+// Example: http://localhost:4528/getimage?author=<AUTHOR_NAME_HERE>&track=<TRACK_NAME_HERE>
+app.get('/api/getimage', (req, res) => {
+  const author = req.query.author;
+  const track = req.query.track;
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  Promise.resolve(functions.getImage(author, track)).then((result)=> res.send(result));
+});
 
-const new_grid = document.getElementById("new_arrivals");
-const popular_grid = document.getElementById("popular");
+app.get('/api/getArtistsAndNames', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  Promise.resolve(functions.getArtistsAndNames()).then((result)=> res.send(result));
+});
 
-async function addImages(data) {
-    const imagesArr = data.map(({ name, song }) => getImage(name, song));
-    const images = await Promise.all(imagesArr);
+// Start the server
+app.listen(port, () => {
+  console.log(`Backend is running at http://localhost:${port}`);
+});
 
-    const updatedData = data.map(( element, index ) => ({
-        ...element,
-        image: images[index],
-    }));
-
-    data.forEach((element, index) => {
-        const image_block = document.getElementById(`${element.song}_image`)
-        if (image_block != undefined) {
-            image_block.setAttribute("style", "background-image: url(" + images[index] + ")")
-        } else {
-            console.log(element.song, element.name, "does not exist!")
-        }
-    })
-
-    return updatedData
-}
-
-async function render() {
-    const data = await loadArtistList(); // no images
-    // const data = await addImages(no_images);
-    const new_slice = data.slice(0, 5);
-    const popular_slice = data.slice(5, 10);
-
-    // console.log(new_slice)
-    // console.log(popular_slice)
-
-    fill_grid(new_grid, new_slice);
-    fill_grid(popular_grid, popular_slice);
-
-    addImages(data)
-}
-
-render();
+module.exports = app;
